6b63a3163e05d697559a2aa5b8ed1d9b95d1bb60
alifamarsya <syafaskincare@gmail.com>
2026-08-07 14:50:49 +0800

    fix: melengkapi dependency pengujian aplikasi

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,42 +2,43 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-#[Fillable([
-    'username',
-    'password',
-    'nama_lengkap',
-    'email',
-    'jabatan',
-    'bagian',
-    'role_id',
-    'is_active',
-    'must_change_pwd',
-    'last_login',
-])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $table = 'users';
+
+    protected $fillable = [
+        'username',
+        'password',
+        'nama_lengkap',
+        'email',
+        'jabatan',
+        'bagian',
+        'role_id',
+        'is_active',
+        'must_change_pwd',
+        'last_login'
+    ];
+
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+        'is_active' => 'boolean',
+        'must_change_pwd' => 'boolean',
+        'last_login' => 'datetime'
         ];
+    }
+    use Notifiable;
+
+    protected $hidden = ['password', 'rememberToken'];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
     }
 }
