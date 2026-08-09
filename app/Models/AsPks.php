07f08d23d4c1fc0da05ff6df80df0f61c5d74d5d
alifamarsya <syafaskincare@gmail.com>
2026-08-09 14:13:01 +0800

    feat: memperbarui model modul aset dan logistik

--- app/Models/AsPks.php
+++ app/Models/AsPks.php
@@ -6,5 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class AsPks extends Model
 {
-    //
+    protected $table = 'as_pks';
+
+    protected $fillable = [
+        'no_pks',
+        'judul',
+        'vendor',
+        'div_owner',
+        'tanggal_mulai',
+        'jatuh_tempo',
+        'nilai',
+        'dokumen',
+        'status',
+        'memo_dibuat',
+        'maker_id',
+        'checker_id',
+        'approval_status',
+        'approved_at',
+        'keterangan'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+        'tanggal_mulai' => 'date',
+        'jatuh_tempo' => 'date',
+        'nilai' => 'decimal:2',
+        'memo_dibuat' => 'boolean',
+        'approved_at' => 'datetime'
+        ];
+    }
+
+    public function maker()
+    {
+        return $this->belongsTo(User::class, 'maker_id');
+    }
+
+    public function checker()
+    {
+        return $this->belongsTo(User::class, 'checker_id');
+    }
 }
