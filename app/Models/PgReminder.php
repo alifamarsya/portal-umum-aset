8616c0a31c6c116d0fd8e2b9a05895df52c2e836
alifamarsya <syafaskincare@gmail.com>
2026-08-09 14:53:03 +0800

    feat: memperbarui model modul pengadaan

--- app/Models/PgReminder.php
+++ app/Models/PgReminder.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class PgReminder extends Model
 {
-    //
+    protected $table = 'pg_reminder';
+
+    protected $fillable = [
+        'judul',
+        'kategori',
+        'tanggal_jatuh_tempo',
+        'status',
+        'catatan'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+        'tanggal_jatuh_tempo' => 'date'
+        ];
+    }
 }
