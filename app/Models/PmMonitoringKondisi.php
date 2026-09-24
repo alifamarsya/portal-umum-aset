143988274a6c11c3cb93f674784f271c9b247866
Dirli12 <dirlydwip@gmail.com>
2026-09-24 08:42:24 +0800

    feat(models): implementasi model arsip dokumen legalitas pks dan jadwal pemeliharaan berkala

---
+++ app/Models/PmMonitoringKondisi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PmMonitoringKondisi extends Model
+{
+    protected $table = 'pm_monitoring_kondisi';
+    protected $guarded = ['id'];
+}
